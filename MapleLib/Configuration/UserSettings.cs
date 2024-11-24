2d7140480005e52d0a9c5313053ce97dc5d07d9d
sirdanielot <daniel@jandey.com>
2024-11-24 13:25:55 +1100

    feat: New option for ignoring .wz extension on IMG export folders

--- MapleLib/Configuration/UserSettings.cs
+++ MapleLib/Configuration/UserSettings.cs
@@ -74,5 +74,8 @@ namespace MapleLib.Configuration
 
         [JsonProperty(PropertyName = "AutoloadRelatedWzFiles")]
         public bool AutoloadRelatedWzFiles = false;
+
+        [JsonProperty(PropertyName = "ExcludeWzImgExport")]
+        public bool ExcludeWzImgExport = false;
     }
 }
